6950c1a16f421def61f1d26fa24da02152b0a5d6
ohinhyuk <8156217@naver.com>
2023-07-28 13:40:39 +0900

    feat : 회원 서비스 개발

---
+++ src/main/java/com/hackathonOne/hackathon/service/MemberService.java
@@ -0,0 +1,41 @@
+package com.hackathonOne.hackathon.service;
+
+import com.hackathonOne.hackathon.domain.entity.Member;
+import com.hackathonOne.hackathon.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public Long join(Member member){
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+
+        Member findMember = memberRepository.findOne(member.getId());
+        if(findMember == null){
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    public Member findOne(Long memberId){
+        return memberRepository.findOne(memberId);
+    }
+
+//    public void update(Long id , String name) {
+//        Member member = memberRepository.findOne(id);
+//        member.setName(name);
+//    }
+}
---
+++ src/test/java/com/hackathonOne/hackathon/service/MemberServiceTest.java
@@ -0,0 +1,16 @@
+package com.hackathonOne.hackathon.service;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+}
