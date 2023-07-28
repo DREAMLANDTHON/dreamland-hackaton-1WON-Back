cff0d0f7ec513a3ec96e987f374e2930c33357f6
ohinhyuk <8156217@naver.com>
2023-07-28 17:36:15 +0900

    feat : 회원 가입후 알러지 관련 추가 정보 입력 api 완료

--- src/main/java/com/hackathonOne/hackathon/controller/memberController.java
+++ src/main/java/com/hackathonOne/hackathon/controller/memberController.java
@@ -1,6 +1,10 @@
 package com.hackathonOne.hackathon.controller;
 
 import com.hackathonOne.hackathon.domain.entity.Member;
+import com.hackathonOne.hackathon.domain.request.AdditionalSignUpRequest;
+import com.hackathonOne.hackathon.domain.request.CreateMemberFirstRequest;
+import com.hackathonOne.hackathon.domain.response.AdditionalSignUpResponse;
+import com.hackathonOne.hackathon.domain.response.CreateMemberFirstResponse;
 import com.hackathonOne.hackathon.service.MemberService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -14,6 +18,9 @@ public class memberController {
 
     private final MemberService memberService;
 
+    /**
+     * 최초 로그인 시 유저 정보 저장
+     */
     @PostMapping("/api/user/first")
     public CreateMemberFirstResponse saveMemberFirst(@RequestBody @Valid CreateMemberFirstRequest request){
         Member member = new Member();
@@ -23,6 +30,19 @@ public class memberController {
 
         Long id = memberService.join(member);
 
-        return new CreateMemberResponse(id);
+        return new CreateMemberFirstResponse(id);
     }
+
+    /**
+     * 추가 회원 정보 입력 받기
+     */
+    @PostMapping("/api/user")
+    public AdditionalSignUpResponse additionalSignUp(@RequestBody @Valid AdditionalSignUpRequest request){
+        memberService.updateMemberHealthInfo(request.getId() ,request.getAllergies(), request.getSpecialTypes());
+        Member newMember = memberService.findOne(request.getId());
+
+        return new AdditionalSignUpResponse(newMember.getId());
+    }
+
+
 }
--- src/main/java/com/hackathonOne/hackathon/domain/entity/Allergy.java
+++ src/main/java/com/hackathonOne/hackathon/domain/entity/Allergy.java
@@ -1,6 +1,7 @@
 package com.hackathonOne.hackathon.domain.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
