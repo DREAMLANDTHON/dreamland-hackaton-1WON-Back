fe87c9a95f6c013b9a6f1d1cf53d00a76c540eb7
ohinhyuk <8156217@naver.com>
2023-07-28 11:27:26 +0900

    feat : 회원 도메인 개발

--- src/main/java/com/hackathonOne/hackathon/domain/Member.java
+++ src/main/java/com/hackathonOne/hackathon/domain/Member.java
@@ -1,4 +1,55 @@
 package com.hackathonOne.hackathon.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+
+@Entity
+@Getter @Setter
 public class Member {
+
+    @Id @GeneratedValue
+    @Column(name="member_id")
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    @OneToMany(mappedBy="member", cascade = CascadeType.ALL)
+        private List<Allergy> allergies = new ArrayList<>();
+    @OneToMany(mappedBy="member", cascade = CascadeType.ALL)
+        private List<CanEat> canEats = new ArrayList<>();
+    @OneToMany(mappedBy="member", cascade = CascadeType.ALL)
+        private List<SpecialType> specialTypes = new ArrayList<>();
+
+    //== 연관관계 메서드 ==//
+//    public void addCanEat(CanEat canEat){
+//        CanEats.add(canEat);
+//        CanEat.setMember(this);
+//    }
+        public void addAllergy(Allergy allergy){
+        Allergies.add(allergy);
+        Allergy.setMember(this);
+    }
+        public void addSpecialType(SpecialType specialType){
+        SpecialTypes.add(specialType);
+        SpecialType.setMember(this);
+    }
+
+    public static Member createMember(Member member, SpecialType... specialTypes, Allergy... allergies){
+
+        for(SpecialType specialType : specialTypes){
+            member.addSpecialType(specialType);
+        }
+
+        for(Allergy allergy : allergies){
+            member.addAllergy(allergy);
+        }
+
+        return member;
+    }
+
 }
